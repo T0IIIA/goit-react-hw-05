c84accce523f2ade3b0057d38fa167b9802d3ea1
Anton Gatsko <89578801+T0IIIA@users.noreply.github.com>
2024-08-03 19:38:28 +0300

    change app directory

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
+import App from './App.jsx'
 import 'modern-normalize'
 import './style.css'
 import { BrowserRouter } from 'react-router-dom'
