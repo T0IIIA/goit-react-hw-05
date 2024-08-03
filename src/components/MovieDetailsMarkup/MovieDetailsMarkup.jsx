6849d8e7dbca0578ad69927b41b99188260e1987
Anton Gatsko <89578801+T0IIIA@users.noreply.github.com>
2024-08-04 01:32:13 +0300

    checked images

--- src/components/MovieDetailsMarkup/MovieDetailsMarkup.jsx
+++ src/components/MovieDetailsMarkup/MovieDetailsMarkup.jsx
@@ -6,7 +6,7 @@ import s from './MovieDetailsMarkup.module.css'
 const MovieDetailsMarkup = ({ movie }) => {
   // const backLink = location.state?.from ?? '/'
   const defaultImg =
-    '<https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg>'
+    'https://dl-media.viber.com/10/share/2/long/vibes/icon/image/0x0/95e0/5688fdffb84ff8bed4240bcf3ec5ac81ce591d9fa9558a3a968c630eaba195e0.jpg'
   
   const movieName = movie.title
   const overview = movie.overview
